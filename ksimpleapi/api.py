e93c0a4c5559b012a50c3fbefaef9055597408a3
kkristof200 <kovacskristof200@gmail.com>
2020-11-11 22:21:07 +0200

    new version

--- ksimpleapi/api.py
+++ ksimpleapi/api.py
@@ -60,8 +60,8 @@ class Api:
         """
 
         return {
-            # 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-            # 'Accept-Encoding': 'gzip, deflate, br',
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
             'Accept-Language': 'en-US,en;q=0.5',
             'Connection': 'keep-alive',
             'DNT': '1',
